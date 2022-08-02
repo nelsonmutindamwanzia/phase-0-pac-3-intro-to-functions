f4abd0fd74171a81d86d79cc996ab936fd559289
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-02 19:44:43 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Follow along with the examples here
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+}
+console.log(say("Howdy", "partner"));
